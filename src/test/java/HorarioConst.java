4f153f718537396203b691dcc2153f759af54e51
natachacgr <natacha.calixto@ges.inatel.br>
2024-09-12 21:22:31 -0300

    classe para test

---
+++ src/test/java/HorarioConst.java
@@ -0,0 +1,22 @@
+public class HorarioConst {
+    public static String HORARIO1=
+            "{\"nomeProfessor\": \"João\", \n" +
+                    "\"horarioDeAtendimento\": \"Segunda-feira 10:00\"} \n" +
+                    "\"periodo\": \"integral\" \n" +
+                    "\"sala\": \"1\" \n" +
+                    "\"predio\": [\"1\", \"2\"]";
+
+    public static String HORARIO2=
+            "{\"nomeProfessor\": \"Maria\", \n" +
+                    "\"horarioDeAtendimento\": \"Terça-feira 14:00\"} \n" +
+                    "\"periodo\": \"noturno\" \n" +
+                    "\"sala\": \"2\" \n" +
+                    "\"predio\": [\"3\", \"4\"]";
+
+    public static String HORARIO3=
+            "{\"nomeProfessor\": \"José\", \n" +
+                    "\"horarioDeAtendimento\": \"Quarta-feira 16:00\"} \n" +
+                    "\"periodo\": \"matutino\" \n" +
+                    "\"sala\": \"3\" \n" +
+                    "\"predio\": [\"5\", \"6\"]";
+}
